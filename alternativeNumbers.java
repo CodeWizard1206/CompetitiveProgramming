954ea75bd7b3abc8b6f3e0441124be0649d65589
CodeWizard1206 <vikaspc786@gmail.com>
2021-10-19 12:03:14 -0700

    Code Updates...

--- alternativeNumbers.java
+++ alternativeNumbers.java
@@ -1,31 +1,35 @@
 import java.util.Scanner;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class alternativeNumbers {
 
-    public static void solution(int[] arr) {
+    public static void solution(List<Integer> arr) {
         Set<Integer> set = new HashSet<Integer>();
 
-        for (int i : arr) {
-            if (set.contains(new Integer(i)) or i < 0) {
+        for (Integer i : arr) {
+            if (set.contains(i) || i < 0) {
                 System.out.println("Invalid Input");
                 return;
             } else {
-                set.add(new Integer(i));
+                set.add(i);
             }
         }
         
+        Collections.sort(arr);
         int l = 0;
-        int h = arr.length - 1;
+        int h = arr.size() - 1;
         int i = 0;
 
-        while (l < h) {
+        while (l <= h) {
             if (i%2 == 0) {
-                System.out.print(arr[h] + " ");
+                System.out.print(arr.get(h) + " ");
                 h--;
             } else {
-                System.out.print(arr[l] + " ");
+                System.out.print(arr.get(l) + " ");
                 l++;
             }
             i++;
@@ -35,12 +39,12 @@ public class alternativeNumbers {
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        int arr[] = new int[n];
+        List<Integer> list = new ArrayList<Integer>();
 
         for (int i = 0; i < n; ++i) {
-            arr[i] = s.nextInt();
+            list.add(s.nextInt());
         }
 
-        solution(arr);
+        solution(list);
     }
 }
