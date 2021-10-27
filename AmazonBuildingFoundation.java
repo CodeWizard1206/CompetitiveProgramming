5709621f606c677bc02759d8e64277b36246a12c
CodeWizard1206 <vikaspc786@gmail.com>
2021-10-27 12:43:02 +0530

    Commit #36 : Adding New Problem Statement

---
+++ AmazonBuildingFoundation.java
@@ -0,0 +1,10 @@
+You are in charge of preparing a recently purchased lot for one of Amazon's new
+building. The lot is covered with trenches and has a single obstacle that needs
+to be taken down before the foundation can be prepared for the building. The
+demolition robot must remove the obstacle before progress can be made on the building.abstract
+
+Write an algorithm to determine the minimum distance required for the demolition
+robot to remove the obstacle.
+
+Assumptions:
+
